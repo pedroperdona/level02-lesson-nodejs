0289699259ee5c0a3322deec260eece7835a7759
Pedro Ernesto Perdoná Rech <p.perdona@softplan.com.br>
2020-06-22 20:23:54 -0300

    first commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,24 @@
+import { parseISO, startOfHour } from 'date-fns';
+import { Router } from 'express';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+  const parsedDate = startOfHour(parseISO(date));
+
+  const existsAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
+
+  if (existsAppointmentInSameDate) {
+    return response.status(400).json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentsRepository.create(provider, parsedDate);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
