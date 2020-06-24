fe9894514a39981fa2817bd734126c245a23728d
Pedro Ernesto Perdoná Rech <p.perdona@softplan.com.br>
2020-06-24 19:48:18 -0300

    add services

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,6 +1,7 @@
 import { parseISO, startOfHour } from 'date-fns';
 import { Router } from 'express';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentsService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
@@ -10,19 +11,19 @@ appointmentsRouter.get('/', (request, response) => {
 });
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
+    const parsedDate = parseISO(date);
 
-  const existsAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
+    const createAppointmentService = new CreateAppointmentsService(appointmentsRepository);
 
-  if (existsAppointmentInSameDate) {
-    return response.status(400).json({ message: 'This appointment is already booked' });
+    const appointment = createAppointmentService.execute({ date: parsedDate, provider });
+
+    return response.json(appointment);
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
   }
-
-  const appointment = appointmentsRepository.create({ provider, date: parsedDate });
-
-  return response.json(appointment);
 });
 
 export default appointmentsRouter;
